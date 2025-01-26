5db09a587358fa309b757f083e9a51ffa4722986
xiangan <anxiangsir@outlook.com>
2025-01-26 21:56:46 +0800

    Add ViT-bigG model implementation
    - Initialized mlcd_rope2d module to support ViT-bigG architecture.

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -5,6 +5,7 @@ from .open_clip_encoder import OpenCLIPVisionTower
 from .hf_vision import HFVisionTower
 from .siglip_encoder import SigLipVisionTower
 from .clip_encoder import CLIPVisionTower, CLIPVisionTowerS2
+from .mlcd_encoder import MLCDVisionTower, MLCDVisionTowerS2
 
 # from .eva_clip.eva_clip_encoder import EvaClipVisionTower
 # from .dev_eva_clip.eva_vit import EvaViTWrapper
@@ -14,12 +15,20 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, "mm_vision_tower", getattr(vision_tower_cfg, "vision_tower", None))
     is_absolute_path_exists = os.path.exists(vision_tower)
     use_s2 = getattr(vision_tower_cfg, "s2", False)
-    if "clip" in vision_tower or "mlcd" in vision_tower or "unicom" in vision_tower or vision_tower.startswith("openai") \
+
+    if "mlcd-vit-bigG-patch14-336" in vision_tower:
+        if use_s2:
+            return MLCDVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
+        else:
+            return MLCDVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+
+    elif "clip" in vision_tower or "mlcd" in vision_tower or "unicom" in vision_tower or vision_tower.startswith("openai") \
         or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower or vision_tower.startswith("DeepGlint"):
         if use_s2:
             return CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
         else:
             return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    
     elif "siglip" in vision_tower:
         return SigLipVisionTower(vision_tower, vision_tower_cfg=vision_tower_cfg, **kwargs)
     elif vision_tower.startswith("hf:"):
