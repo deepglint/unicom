c55d2a4e54467abdb5422e1d3d187f45f4f0fc74
Xiang An <anxiangsir@outlook.com>
2025-04-06 12:13:22 +0800

    Revert "MLCD_VL (#131)"
    This reverts commit 965cc38248ef33a031105fe0ec4074142925bfde.

--- scripts/pretrain_mlcd.sh
+++ scripts/pretrain_mlcd.sh
@@ -1,3 +1,6 @@
+export OMP_NUM_THREADS=8
+export NCCL_IB_DISABLE=0
+export NCCL_IB_GID_INDEX=3
 export NCCL_SOCKET_IFNAME=eth0
 export NCCL_DEBUG=INFO
 export NUM_GPUS=8
@@ -53,5 +56,4 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --dataloader_num_workers 16 \
     --lazy_preprocess True \
     --report_to wandb \
-    --run_name $BASE_RUN_NAME \
-    --attn_implementation sdpa
+    --run_name $BASE_RUN_NAME 
